baa4771d41729c5953500a97003c84a91e8f6d2f
mmizab <mohammed.mizab.latrache@gmail.com>
2020-12-24 10:29:51 +0100

    add menu for small media with javascript event listeners

---
+++ WebApp/wwwroot/js/Home/index.js
@@ -0,0 +1,30 @@
+﻿function toggleMenu(x) {
+    if (x.matches) { // If media query matches
+        var menu = document.getElementById("toggle-menu-list");
+        menu.style.display = "none";
+        var nav = document.getElementById("toggle-menu-show");
+        nav.addEventListener("click", ShowMenu);
+    } else {
+        var menu = document.getElementById("toggle-menu-list");
+        menu.style.display = "block";
+        var nav = document.getElementById("toggle-menu-show");
+        nav.removeEventListener("click", ShowMenu);
+    }
+}
+
+function ShowMenu(event) {
+    var menu = document.getElementById("toggle-menu-list");
+    if (menu.style.display == "none") {
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    var x = window.matchMedia("(max-width: 480px)")
+    toggleMenu(x) // Call listener function at run time
+    x.addListener(toggleMenu) // Attach listener function on state changes 
+
+
+});
