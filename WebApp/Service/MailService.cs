05fa4a18b3af852f0340155b8d37b716b873dd94
mmizab <mohammed.mizab.latrache@gmail.com>
2020-12-26 19:02:07 +0100

    comment mail service

--- WebApp/Service/MailService.cs
+++ WebApp/Service/MailService.cs
@@ -9,27 +9,27 @@ namespace WebApp.Service
 {
     public class MailService
     {
-        MailAddress fromAddress = new MailAddress("");
-        MailAddress toAddress = new MailAddress("");
-        const string fromPassword = "";
-        const string subject = "Curriculum";
+        //MailAddress fromAddress = new MailAddress("");
+        //MailAddress toAddress = new MailAddress("");
+        //const string fromPassword = "";
+        //const string subject = "Curriculum";
 
-        public void SendMail(string body) {
-            var smtp = new SmtpClient
-            {
-                Host = "smtp.gmail.com",
-                Port = 587,
-                EnableSsl = true,
-                DeliveryMethod = SmtpDeliveryMethod.Network,
-                UseDefaultCredentials = false,
-                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
-            };
-            using var message = new MailMessage(fromAddress, toAddress)
-            {
-                Subject = subject,
-                Body = body
-            };
-            smtp.Send(message);
-        }
+        //public void SendMail(string body) {
+        //    var smtp = new SmtpClient
+        //    {
+        //        Host = "smtp.gmail.com",
+        //        Port = 587,
+        //        EnableSsl = true,
+        //        DeliveryMethod = SmtpDeliveryMethod.Network,
+        //        UseDefaultCredentials = false,
+        //        Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+        //    };
+        //    using var message = new MailMessage(fromAddress, toAddress)
+        //    {
+        //        Subject = subject,
+        //        Body = body
+        //    };
+        //    smtp.Send(message);
+        //}
     }
 }
